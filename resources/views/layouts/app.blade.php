5dc965e225af4b53a3d62f1b857aa9fb218d68df
evanmunasinghe <esmunasinghe@gmail.com>
2026-06-22 19:59:07 +0530

    components are learned

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,7 @@
-@extends('layouts.clean')
+@props(['title' => ''])
 
+<x-base-layout :$title>
+<x-layouts.header> </x-layouts.header>
+    {{ $slot }}
 
-@section('childContent')
-@include('layouts.partials.header')
-    @yield('content')
-    <footer>
-      @section('footerLinks')
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      @show
-    </footer>
-@endsection
+</x-base-layout>
